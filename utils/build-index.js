fdde8b055e23946c820cd16a34f0946253614f84
Brian Broll <brian.broll@gmail.com>
2021-12-10 11:17:45 -0600

    Add precommit hook

---
+++ utils/build-index.js
@@ -0,0 +1,40 @@
+const _ = require('lodash');
+const fs = require('fs');
+const path = require('path');
+const EXTENSIONS_DIR = path.join(__dirname, '..', 'extensions');
+const TEMPLATES_DIR = path.join(__dirname, 'templates');
+
+const extensions = fs.readdirSync(EXTENSIONS_DIR)
+    .map(readExtension);
+const templateData = {extensions};
+
+const updateCount = fs.readdirSync(TEMPLATES_DIR)
+    .reduce((updateCount, name) => renderTemplate(name, templateData) + updateCount, 0)
+
+if (updateCount > 0) {
+    process.exit(1);
+}
+
+function readExtension(name) {
+    const dirpath = path.join(EXTENSIONS_DIR, name);
+    const description = fs.readFileSync(path.join(dirpath, 'description.txt'), 'utf8');
+    const url = `https://dev.netsblox.org/?extensions=[%22https://extensions.netsblox.org/extensions/${name}/index.js%22]#`
+    return {
+        name,
+        description,
+        url,
+    };
+}
+
+function renderTemplate(name, data) {
+    const template = fs.readFileSync(path.join(TEMPLATES_DIR, name), 'utf8');
+    const outpath = path.join(__dirname, '..', name);
+    const newContent = _.template(template)(data).trim();
+    const content = fs.readFileSync(outpath, 'utf8').trim()
+    if (newContent !== content) {
+        fs.writeFileSync(outpath, newContent);
+        console.log('updated', name);
+        return 1;
+    }
+    return 0;
+}
