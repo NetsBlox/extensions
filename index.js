31f9cdc7f9ebee5aa61d64ea237f41b1ec352df7
Gordon Stein <gstein@ltu.edu>
2022-05-30 14:46:15 -0500

    Test form js

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
-document.querySelector("#loadButton").onclick = () => {
-  let url = "";
-
-  const selectedBoxes = Array.from(
-    document.querySelectorAll("input.extensionOption")
-  ).filter((checkbox) => checkbox.selected);
-
-  for (const selectedBox of selectedBoxes) {
-    console.log(selectedBox);
-  }
-  if (!document.querySelector("#devMode").checked) {
-    url = `https://editor.netsblox.org/?extensions=[%22${scriptUrl}%22]#`;
-  } else {
-    url = `https://dev.netsblox.org/?extensions=[%22${scriptUrl}%22]#`;
-  }
-
-  console.log(url);
+const getSelectedExtensions = () => {
+    return Array.from(document.querySelectorAll("input.extensionOption")).filter((checkbox) => checkbox.checked);
 };
+
+document.getElementById("multiSelect").onchange = () => {
+    if (document.getElementById("multiSelect").checked) {
+        // Show multi-select interface
+        Array.from(document.getElementsByClassName("extensionOption")).forEach((element) => (element.hidden = false));
+        document.getElementById("multiSelectControls").hidden = false;
+    } else {
+        // Hide multi-select interface
+        Array.from(document.getElementsByClassName("extensionOption")).forEach((element) => (element.hidden = true));
+        document.getElementById("multiSelectControls").hidden = true;
+    }
+};
+
+document.querySelectorAll("input.extensionOption").forEach((option) => {
+    option.oninput = () => {
+        document.getElementById("loadButton").disabled = getSelectedExtensions().length == 0;
+    };
+});
+
+document.getElementById("loadButton").onclick = () => {
+    let url = "";
+
+    let scriptUrl = "";
+
+    for (const selectedBox of getSelectedExtensions()) {
+        scriptUrl += "%22" + document.getElementById(selectedBox.name + "_script").value + "%22,";
+    }
+
+    // Remove trailing comma
+    scriptUrl = scriptUrl.substring(0, scriptUrl.length - 1);
+
+    if (!document.getElementById("devMode").checked) {
+        url = `https://editor.netsblox.org/?extensions=[${scriptUrl}]#`;
+    } else {
+        url = `https://dev.netsblox.org/?extensions=[${scriptUrl}]#`;
+    }
+
+    window.open(url);
+};
+
+document.getElementById("loadButton").disabled = getSelectedExtensions().length == 0;
+document.getElementById("multiSelect").onchange();
