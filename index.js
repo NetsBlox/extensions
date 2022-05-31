cb5dbf020e46522816f0e2273ea03cb68fdf0fae
Gordon Stein <7331488+gsteinLTU@users.noreply.github.com>
2022-05-31 20:43:16 +0000

    Select Multiple extensions to load together (#10)
    * Test form
    
    * Test form js
    
    * Test form js
    
    * Test form js
    
    * Hide controls by default

---
+++ index.js
@@ -0,0 +1,45 @@
+const getSelectedExtensions = () => {
+    return Array.from(document.querySelectorAll("input.extensionOption")).filter((checkbox) => checkbox.checked);
+};
+
+document.getElementById("multiSelect").onchange = () => {
+    if (document.getElementById("multiSelect").checked) {
+        // Show multi-select interface
+        Array.from(document.getElementsByClassName("extensionOption")).forEach((element) => (element.hidden = false));
+        document.getElementById("multiSelectControls").hidden = false;
+    } else {
+        // Hide multi-select interface
+        Array.from(document.getElementsByClassName("extensionOption")).forEach((element) => (element.hidden = true));
+        document.getElementById("multiSelectControls").hidden = true;
+    }
+};
+
+document.querySelectorAll("input.extensionOption").forEach((option) => {
+    option.oninput = () => {
+        document.getElementById("loadButton").disabled = getSelectedExtensions().length == 0;
+    };
+});
+
+document.getElementById("loadButton").onclick = () => {
+    let url = "";
+
+    let scriptUrl = "";
+
+    for (const selectedBox of getSelectedExtensions()) {
+        scriptUrl += "%22" + document.getElementById(selectedBox.name + "_script").value + "%22,";
+    }
+
+    // Remove trailing comma
+    scriptUrl = scriptUrl.substring(0, scriptUrl.length - 1);
+
+    if (!document.getElementById("devMode").checked) {
+        url = `https://editor.netsblox.org/?extensions=[${scriptUrl}]#`;
+    } else {
+        url = `https://dev.netsblox.org/?extensions=[${scriptUrl}]#`;
+    }
+
+    window.open(url);
+};
+
+document.getElementById("loadButton").disabled = getSelectedExtensions().length == 0;
+document.getElementById("multiSelect").onchange();
