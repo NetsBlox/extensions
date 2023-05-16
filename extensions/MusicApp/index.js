bf0f314f1da77bfbdb6c77b6473786c2c1f5e784
Oluwatito Ebiwonjumi <31973148+ebiwonjumit@users.noreply.github.com>
2023-05-16 15:38:56 -0500

    Removed unnecessary blocks

--- extensions/MusicApp/index.js
+++ extensions/MusicApp/index.js
@@ -33,11 +33,6 @@
                 new Extension.Palette.Block('track'),
                 new Extension.Palette.Block('masterVolume'),
                 new Extension.Palette.Block('trackVolume'),
-                new Extension.Palette.Block('playAll'),
-                new Extension.Palette.Block('stopAll'),
-                new Extension.Palette.Block('pauseAll'),
-                new Extension.Palette.Block('resumeAll'),
-                new Extension.Palette.Block('loopForever'),
             ];
             return [
                 new Extension.PaletteCategory('music', blocks, SpriteMorph),
@@ -71,31 +66,6 @@
                     },{ args: [], timeout: I32_MAX });
                 }),
                 block('trackVolume', 'command', 'music', 'track volume %n %', ['50'], function (value){
-                    this.runAsyncFn(async () =>{
-                        playAudio();
-                    },{ args: [], timeout: I32_MAX });
-                }),
-                block('playAll', 'command', 'music', 'play all', [], function (){
-                    this.runAsyncFn(async () =>{
-                        playAudio();
-                    },{ args: [], timeout: I32_MAX });
-                }),
-                block('stopAll', 'command', 'music', 'stop all', [], function (){
-                    this.runAsyncFn(async () =>{
-                        playAudio();
-                    },{ args: [], timeout: I32_MAX });
-                }),
-                block('pauseAll', 'command', 'music', 'pause all', [], function (){
-                    this.runAsyncFn(async () =>{
-                        playAudio();
-                    },{ args: [], timeout: I32_MAX });
-                }),
-                block('resumeAll', 'command', 'music', 'resume all', [], function (){
-                    this.runAsyncFn(async () =>{
-                        playAudio();
-                    },{ args: [], timeout: I32_MAX });
-                }),
-                block('loopForever', 'command', 'music', 'loop forever', [], function (){
                     this.runAsyncFn(async () =>{
                         playAudio();
                     },{ args: [], timeout: I32_MAX });
