26e6cc50309cfd357b01a7e0d607d8f48bb97c97
Oluwatito Ebiwonjumi <31973148+ebiwonjumit@users.noreply.github.com>
2023-06-01 15:13:48 -0500

    Playing clips and checking tracks. No more C-block

--- extensions/MusicApp/index.js
+++ extensions/MusicApp/index.js
@@ -105,8 +105,8 @@
          clipSource.connect(this.audioSink);
          clipSource.onended = this.#sourceEnded.bind(this, clipSource, null);
          this.#audioSources.push(clipSource);
-         return clipSource.start(startTime);
-         // return audioBuffer.duration;
+         clipSource.start(startTime);
+         return audioBuffer.duration;
       }
 
       async playFile(file, startTime) {
@@ -785,6 +785,9 @@
             delete this.#tracks[trackName];
          }
       }
+      getTrack(trackName) {
+         return this.#tracks[trackName];
+      }
     
       deleteAllTracks() {
          this.volumeControl.gain.setTargetAtTime(0.0, this.audioContext.currentTime, 0.01);
@@ -870,16 +873,35 @@
       const audioAPI = new WebAudioAPI();
        const I32_MAX = 2147483647;
        audioAPI.start();
-       const backgroundTrack = audioAPI.createTrack("backgroundTrack");
+       const hiddenTrack = audioAPI.createTrack("backgroundTrack");
    //make invisble track to play clip without REAL track 
 
-       async function playAudio(buffer){
-           console.log(`I AM PLAYING AUDIO`);
-           await backgroundTrack.playFile("http://localhost:8000/extensions/MusicApp/AK_UNDOG_ACOUSTIC_GUITAR_4.mp3",0);
+       async function playAudio(buffer, trackName){
+           console.log(`HERE IS YOUR CURRENT TRACK NAME: ${trackName}`);
+           audioAPI.start();
+           if(trackName === undefined){
+               console.log(`I AM PLAYING AUDIO`);
+               return hiddenTrack.playFile("http://localhost:8000/extensions/MusicApp/AK_UNDOG_ACOUSTIC_GUITAR_4.mp3",0);
+
+           }
+           else {
+               console.log(`I MADE IT INTO GET TRACK`);
+               return audioAPI.getTrack(trackName).playFile(buffer, 0);
+           }
+           
        }
        function createTrack(trackName){
-           console.log(`I MADE A TRACK`);
            audioAPI.createTrack(trackName);
+           console.log(`I MADE A TRACK`);
+       }
+
+       function stopAudio(){
+           return audioAPI.stop();
+       }
+       async function wait(duration) {
+           return new Promise(resolve => {
+               setTimeout(resolve, duration * 1000);
+           })
        }
        // ----------------------------------------------------------------------
        class MusicApp extends Extension {
@@ -901,6 +923,7 @@
            getPalette() {
                const blocks = [
                    new Extension.Palette.Block('playClip'),
+                   new Extension.Palette.Block('stopClips'),
                    new Extension.Palette.Block('playbackControls'),
                    new Extension.Palette.Block('track'),
                    new Extension.Palette.Block('masterVolume'),
@@ -919,29 +942,36 @@
                return [
                    block('playClip', 'command', 'music', 'play clip %s', [], function (audioBuffer){
                        this.runAsyncFn(async () =>{
-                           playAudio();
+                           const trackName = this.trackName;
+                           const duration = await playAudio(audioBuffer, trackName);
+                           await wait(duration);
+                       },{ args: [], timeout: I32_MAX });
+                   }),
+                   block('stopClips', 'command', 'music', 'stop all clips', [], function (){
+                       this.runAsyncFn(async () =>{
+                           stopAudio();
                        },{ args: [], timeout: I32_MAX });
                    }),
                    block('playbackControls', 'command', 'music', 'playback %s time sig. %bpmNotes BPM = %n', ['4/4', 'Quarter', '120'], function (audio){
                        this.runAsyncFn(async () =>{
-                           playAudio();
+                           playAudio(audio);
                        },{ args: [], timeout: I32_MAX });
                    }),
-                   block('track', 'command', 'music', 'track %s %cs', ['Name'], function (trackName){
+                   block('track', 'command', 'music', 'track %s', ['Name'], function (trackName){
+                       // console.log(`HERE IS THE SECOND INPUT ${underBlock}`);
                        // var block = this.context.expression;
                        // console.log({arguments, "this": this})
-                       this.runAsyncFn(async () =>{
                            createTrack(trackName);
-                       },{ args: [], timeout: I32_MAX });
+                           this.trackName = trackName;
                    }),
                    block('masterVolume', 'command', 'music', 'master volume %n %', ['80'], function (value){
                        this.runAsyncFn(async () =>{
-                           playAudio();
+                           playAudio(value);
                        },{ args: [], timeout: I32_MAX });
                    }),
                    block('trackVolume', 'command', 'music', 'track volume %n %', ['50'], function (value){
                        this.runAsyncFn(async () =>{
-                           playAudio();
+                           playAudio(value);
                        },{ args: [], timeout: I32_MAX });
                    })
                ];
--- extensions/MusicApp/src/index.js
+++ extensions/MusicApp/src/index.js
@@ -4,16 +4,35 @@ import {WebAudioAPI} from "./WebAudioAPI/library/webaudioapi/webAudioAPI";
    const audioAPI = new WebAudioAPI();
     const I32_MAX = 2147483647;
     audioAPI.start();
-    const backgroundTrack = audioAPI.createTrack("backgroundTrack");
+    const hiddenTrack = audioAPI.createTrack("backgroundTrack");
 //make invisble track to play clip without REAL track 
 
-    async function playAudio(buffer){
-        console.log(`I AM PLAYING AUDIO`);
-        await backgroundTrack.playFile("http://localhost:8000/extensions/MusicApp/AK_UNDOG_ACOUSTIC_GUITAR_4.mp3",0);
+    async function playAudio(buffer, trackName){
+        console.log(`HERE IS YOUR CURRENT TRACK NAME: ${trackName}`);
+        audioAPI.start();
+        if(trackName === undefined){
+            console.log(`I AM PLAYING AUDIO`);
+            return hiddenTrack.playFile("http://localhost:8000/extensions/MusicApp/AK_UNDOG_ACOUSTIC_GUITAR_4.mp3",0);
+
+        }
+        else {
+            console.log(`I MADE IT INTO GET TRACK`);
+            return audioAPI.getTrack(trackName).playFile(buffer, 0);
+        }
+        
     }
     function createTrack(trackName){
-        console.log(`I MADE A TRACK`);
         audioAPI.createTrack(trackName);
+        console.log(`I MADE A TRACK`);
+    }
+
+    function stopAudio(){
+        return audioAPI.stop();
+    }
+    async function wait(duration) {
+        return new Promise(resolve => {
+            setTimeout(resolve, duration * 1000);
+        })
     }
     // ----------------------------------------------------------------------
     class MusicApp extends Extension {
@@ -35,6 +54,7 @@ import {WebAudioAPI} from "./WebAudioAPI/library/webaudioapi/webAudioAPI";
         getPalette() {
             const blocks = [
                 new Extension.Palette.Block('playClip'),
+                new Extension.Palette.Block('stopClips'),
                 new Extension.Palette.Block('playbackControls'),
                 new Extension.Palette.Block('track'),
                 new Extension.Palette.Block('masterVolume'),
@@ -53,7 +73,14 @@ import {WebAudioAPI} from "./WebAudioAPI/library/webaudioapi/webAudioAPI";
             return [
                 block('playClip', 'command', 'music', 'play clip %s', [], function (audioBuffer){
                     this.runAsyncFn(async () =>{
-                        playAudio(audioBuffer);
+                        const trackName = this.trackName;
+                        const duration = await playAudio(audioBuffer, trackName);
+                        await wait(duration);
+                    },{ args: [], timeout: I32_MAX });
+                }),
+                block('stopClips', 'command', 'music', 'stop all clips', [], function (){
+                    this.runAsyncFn(async () =>{
+                        stopAudio();
                     },{ args: [], timeout: I32_MAX });
                 }),
                 block('playbackControls', 'command', 'music', 'playback %s time sig. %bpmNotes BPM = %n', ['4/4', 'Quarter', '120'], function (audio){
@@ -61,12 +88,12 @@ import {WebAudioAPI} from "./WebAudioAPI/library/webaudioapi/webAudioAPI";
                         playAudio(audio);
                     },{ args: [], timeout: I32_MAX });
                 }),
-                block('track', 'command', 'music', 'track %s %cs', ['Name'], function (trackName){
+                block('track', 'command', 'music', 'track %s', ['Name'], function (trackName){
+                    // console.log(`HERE IS THE SECOND INPUT ${underBlock}`);
                     // var block = this.context.expression;
                     // console.log({arguments, "this": this})
-                    this.runAsyncFn(async () =>{
                         createTrack(trackName);
-                    },{ args: [], timeout: I32_MAX });
+                        this.trackName = trackName;
                 }),
                 block('masterVolume', 'command', 'music', 'master volume %n %', ['80'], function (value){
                     this.runAsyncFn(async () =>{
