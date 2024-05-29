bcd261f56c643aff80dad02bef229d2a36bcf5c4
Saman Kittani <87401990+samankittani@users.noreply.github.com>
2024-05-29 15:16:45 -0500

    mediapipe versioning (#70)
    add versions to vision module and task cdn's

--- utils/visionModuleLoader.js
+++ utils/visionModuleLoader.js
@@ -16,7 +16,7 @@ Global Vision Constant used by
 mediapipe vision task extensions */
 const Vision = {
   Module: null,
-  Task: null
+  Task: null,
 };
 
 /* 
@@ -24,14 +24,15 @@ This anonymous function
 sets the values for the 
 global vision constants*/
 
-(async function (){
-
-  const VisionModuleURL = 'https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/vision_bundle.mjs';
-  const WASMURL = 'https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/wasm';
+(async function () {
+  const VisionModuleURL =
+    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.14/vision_bundle.mjs";
+  const WASMURL =
+    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-visioni@0.10.14/wasm";
 
   Vision.Module = await import(VisionModuleURL);
   Vision.Task = await Vision.Module.FilesetResolver.forVisionTasks(WASMURL);
-  
-  Object.defineProperty(Vision, "Module", {writable: false}); 
-  Object.defineProperty(Vision, "Task", {writable: false});
-  })();
+
+  Object.defineProperty(Vision, "Module", { writable: false });
+  Object.defineProperty(Vision, "Task", { writable: false });
+})();
