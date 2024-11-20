5fb9d45e0731cd3872fd96907b4fd31b623258e9
Gordon Stein <7331488+gsteinLTU@users.noreply.github.com>
2024-11-20 11:26:16 -0600

    Hack fix for retina support

--- extensions/VariablePlotter/index.js
+++ extensions/VariablePlotter/index.js
@@ -69,21 +69,28 @@
     script.async = false;
     script.setAttribute('src', 'https://pseudomorphic.netsblox.org/script.js');
     script.onload = function() {
-        plotterDialog = createDialog('Variable Plotter');
-        
         // Find <content> element in dialog and add the plotter to it
-        disableRetinaSupport();
+        plotterDialog = createDialog('Variable Plotter');
         plotterInner = document.createElement('canvas');
+        
+        if (isRetinaEnabled()) {
+            var a = HTMLCanvasElement.prototype;
+            var c = a._bak;
+            Object.defineProperty(plotterInner, "width", c.width);
+            Object.defineProperty(plotterInner, "height", c.height);
+        }
+
         plotterInner.style.width = '100%';
         plotterInner.style.height = '100%';
         plotterInner.width = 400;
         plotterInner.height = 400;
         plotterInner.style.minWidth = '400px';
         plotterInner.style.minHeight = '400px';
-
+        
         plotterDialog.querySelector('content').appendChild(plotterInner);
 
         plotterDialog.addEventListener('resize', function() {
+
             plotterInner.width = plotterDialog.clientWidth;
             plotterInner.height = plotterDialog.clientHeight;
             chart?.resize();
@@ -108,10 +115,22 @@
         script.setAttribute('src', 'https://extensions.netsblox.org/extensions/VariablePlotter/chart.js');
     }
     script.onload = function() {
-        chart = new Chart(plotterInner.getContext('2d'), {
+        let plotterContext = plotterInner.getContext('2d');
+
+        if (isRetinaEnabled()) {
+            var a = HTMLCanvasElement.prototype;
+            var c = a._bak;
+            plotterContext.drawImage = c.drawImage;
+            plotterContext.getImageData = c.getImageData;
+            Object.defineProperty(plotterContext, "shadowOffsetX", c.shadowOffsetX);
+            Object.defineProperty(plotterContext, "shadowOffsetY", c.shadowOffsetY);
+            Object.defineProperty(plotterContext, "shadowBlur", c.shadowBlur);
+        }
+
+        chart = new Chart(plotterContext, {
             type: 'bar',
             data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                labels: [1, 2, 3, 4, 5, 6],
                 datasets: [{
                 label: '# of Votes',
                 data: [12, 19, 3, 5, 2, 3],
@@ -130,6 +149,6 @@
         });
     };
     document.head.appendChild(script);
-
+    
     NetsBloxExtensions.register(VariablePlotter);
 })();
