65596c10e6e0601b9dce10046b746fa3003212de
Gordon Stein <7331488+gsteinLTU@users.noreply.github.com>
2024-11-20 10:31:39 -0600

    Switch to chart.js

--- extensions/VariablePlotter/index.js
+++ extensions/VariablePlotter/index.js
@@ -1,6 +1,7 @@
 (function () {    
     var plotterDialog;
     var plotterInner;
+    var chart;
 
     class VariablePlotter extends Extension {
         constructor(ide) {
@@ -51,6 +52,9 @@
 
     }
 
+    function getGlobalVariables() {
+        return document.world.children[0].stage.globalVariables();
+    }
 
     // Add CSS
     var element = document.createElement('link');
@@ -62,15 +66,31 @@
     // Add JS
     var script = document.createElement('script');
     script.type = 'text/javascript';
+    script.async = false;
     script.setAttribute('src', 'https://pseudomorphic.netsblox.org/script.js');
     script.onload = function() {
         plotterDialog = createDialog('Variable Plotter');
         
         // Find <content> element in dialog and add the plotter to it
-        plotterInner = document.createElement('div');
+        disableRetinaSupport();
+        plotterInner = document.createElement('canvas');
+        plotterInner.style.width = '100%';
+        plotterInner.style.height = '100%';
+        plotterInner.width = 400;
+        plotterInner.height = 400;
+        plotterInner.style.minWidth = '400px';
+        plotterInner.style.minHeight = '400px';
+
         plotterDialog.querySelector('content').appendChild(plotterInner);
 
+        plotterDialog.addEventListener('resize', function() {
+            plotterInner.width = plotterDialog.clientWidth;
+            plotterInner.height = plotterDialog.clientHeight;
+            chart?.resize();
+        });
+
         setupDialog(plotterDialog);
+
         externalVariables['plotterDialog'] = plotterDialog;
         externalVariables['plotterInner'] = plotterInner;
     };
@@ -81,65 +101,35 @@
     // Add uPlot
     var script = document.createElement('script');
     script.type = 'text/javascript';
+    script.async = false;
     if(document.currentScript.src.includes('localhost')) {
-        script.setAttribute('src', 'http://localhost:4000/uPlot.iife.min.js');
+        script.setAttribute('src', 'http://localhost:4000/chart.js');
     } else {
-        script.setAttribute('src', 'https://extensions.netsblox.org/extensions/VariablePlotter/uPlot.iife.min.js');
+        script.setAttribute('src', 'https://extensions.netsblox.org/extensions/VariablePlotter/chart.js');
     }
     script.onload = function() {
-        
-        // Define the chart options and initial empty data
-        let opts = {
-            title: "My Chart",
-            id: "chart1",
-            class: "my-chart",
-            width: 800,
-            height: 600,
-            series: [
-                {},
-                {
-                    // initial toggled state (optional)
-                    show: true,
-            
-                    spanGaps: false,
-            
-                    // in-legend display
-                    label: "RAM",
-                    value: (self, rawValue) => "$" + rawValue.toFixed(2),
-            
-                    // series style
-                    stroke: "red",
-                    width: 1,
-                    fill: "rgba(255, 0, 0, 0.3)",
-                    dash: [10, 5],
+        chart = new Chart(plotterInner.getContext('2d'), {
+            type: 'bar',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                y: {
+                    beginAtZero: true
                 }
-            ],
-        };
-
-        // Initial empty data (arrays of x and y values)
-        let data = [
-            [1], // x values (e.g., timestamps or indices)
-            [1]  // y values (data points)
-        ];
-
-        // Initialize the chart
-        setTimeout(() => {
-            const chart = new uPlot(opts, data, plotterInner);
-        }, 1000);
+                },
+                responsive: true,
+                maintainAspectRatio: false,
+            }
+        });
     };
     document.head.appendChild(script);
-
-    var script = document.createElement('link');
-    script.setAttribute('rel', 'stylesheet');
-    script.setAttribute('type', 'text/css');
-    if(document.currentScript.src.includes('localhost')) {
-        script.setAttribute('href', 'http://localhost:4000/uPlot.min.css');
-    } else {
-        script.setAttribute('href', 'https://extensions.netsblox.org/extensions/VariablePlotter/uPlot.min.css');
-    }
-
-    document.head.appendChild(script);
-
 
     NetsBloxExtensions.register(VariablePlotter);
 })();
