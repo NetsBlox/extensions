5d7bd9d714fa18929d3a0bf40ee3baf6b06d8da6
Devin Jean <emailcruzjean@yahoo.com>
2025-04-09 16:33:41 -0500

    even more less

--- extensions/BetterShare/index.js
+++ extensions/BetterShare/index.js
@@ -7,31 +7,8 @@
 
         getMenu() {
             return {
-                'Share Project': async () => {
-                    const res = await this.ide.cloud.publishProject(this.ide?.cloud?.projectId);
-
-                    // TODO: this feels wrong in terms of the cloud response - if ever fixed in cloud, update here
-                    if (res === 'Public') {
-                        this.ide.showMessage('Project must first be saved to the cloud!');
-                        return;
-                    }
-
-                    const username = this.ide?.cloud?.username;
-                    const projName = this.ide?.room?.name;
-                    const roleName = this.ide?.projectName;
-
-                    if (!username || !projName || !roleName) {
-                        this.ide.showMessage('Failed to get shared project info');
-                        return;
-                    }
-
-                    const shareLink = `${location.origin}/?action=present&editMode&noRun&Username=${encodeURIComponent(username)}&ProjectName=${encodeURIComponent(projName)}&Role=${encodeURIComponent(roleName)}`;
-                    new ShareMorph(shareLink).popUp(world);
-                },
-                'Unshare Project': async () => {
-                    await this.ide.cloud.unpublishProject(this.ide?.cloud?.projectId);
-                    this.ide.showMessage('Successfully unpublished your current project');
-                },
+                'Share Project': this.ide.share,
+                'Unshare Project': this.ide.unshare,
             };
         }
     }
